f6b8738f37130736a1bf192641d314460aa64e7c
Andrii Sherepa <asherepa@gmail.com>
2018-03-21 23:52:32 +0200

    Add missing mocks

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 import express from 'express';
-import { graphqlExpress, graphiqlExpress } from "apollo-server-express";
-import bodyParser  from "body-parser";
-import { schema } from "./data/schema";
+import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
+import bodyParser  from 'body-parser';
+import { schema } from './data/schema';
 
 
-var app = express();
-app.use("/graphql", bodyParser.json(), graphqlExpress({ schema }));
-app.use("/graphiql", graphiqlExpress({ endpointURL: "/graphql" }));
-app.listen(4000, () => console.log("Now browse to localhost:4000/graphiql"));
+const app = express();
+app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
+app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
+app.listen(4000, () => console.log('Now browse to localhost:4000/graphiql'));
