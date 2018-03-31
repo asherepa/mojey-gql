72a019fe37dc420264e8b4fb91599a88f73e3dae
Andrii Sherepa <asherepa@gmail.com>
2018-03-31 23:07:34 +0300

    Add account type

--- data/schema.js
+++ data/schema.js
@@ -8,6 +8,20 @@ type Query {
   allTransactions: [Transaction]
 }
 
+type Account {
+  id: ID!
+  name: String!
+  type: AccountType
+}
+
+enum AccountType {
+  ASSET
+  LIABILITY
+  INCOME
+  EXPENSE
+  EQUITY
+}
+
 type Transaction {
   id: ID!
   debit: Float
